c2a8f4fd6ec0e3987275a8ea5dc36b70a8e38c22
SaraCarlsson <sa3575ca-s@student.lu.se>
2019-10-07 15:34:35 +0200

    rensat script

--- script.js
+++ script.js
@@ -60,3 +60,76 @@ function validateForm()
     return (false)
 }
 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
+
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  5000);
+
+
+function openNav() {
+    document.getElementById("myNav2").style.display = "none";
+    document.getElementById("myNav2").style.height = "0%";
+    document.getElementById("myNav").style.display = "block";
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("myNav").style.display = "block";
+  document.getElementById("myNav2").style.display = "block";
+}
+
+function openNav2() {    
+    document.getElementById("myNav").style.display = "none";
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav2").style.display = "block";
+    document.getElementById("myNav2").style.height = "100%";
+}
+
+function closeNav2() {
+  document.getElementById("myNav2").style.height = "0%";
+  document.getElementById("myNav").style.display = "block";
+  document.getElementById("myNav2").style.display = "block";
+}
+
+	const second = 1000,
+     minute = second * 60,
+    hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('May 8, 2022 00:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+        distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+      
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second)
+
