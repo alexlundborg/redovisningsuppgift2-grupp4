0ad78668da2e86559d022e35b66d1140234541e9
Alexander Lundborg <alexander.lundborg@gmail.com>
2019-10-07 16:23:43 +0200

    php-versioner av alla filer i bli-karnevalist-menyn

---
+++ ansok.php
@@ -0,0 +1,57 @@
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+
+<link rel="stylesheet" href="style.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link rel="icon" href="bilder/vieriet.png" type="image/png" sizes="16x16">
+
+</head>
+
+<body>
+
+<?php include 'header.html';?>
+
+<div class="grid-container">
+<div>
+	
+</div>
+
+<main>
+	<h2>Ansök till sektion</h2>
+<p>
+Välj sektion
+</p>
+<select>
+  <option value="AdministerIT">AdministerIT</option>
+  <option value="Biljonsen">Biljonsen</option>
+  <option value="Blädderiet">Blädderiet</option>
+  <option value="Dansen">Dansen</option>
+</select>
+<br />
+<br />
+<form>
+  Förnamn: <br /><input class="text-input" type="text" name="first-name"><br /><br />
+  Efternamn: <br /><input class="text-input" type="text" name="last-name"><br /><br />
+  E-mail: <br /><input class="text-input" type="text" name="mail">
+
+<br /><br />
+
+  <input class="submit" type="submit">
+</form>
+
+
+
+</main>
+
+<div>
+</div>
+</div>
+
+
+
+<?php include 'footer.html';?>
+
+<script src="script.js"></script>
+</body>
---
+++ bladderiet.php
@@ -0,0 +1,38 @@
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+
+<link rel="stylesheet" href="style.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link rel="icon" href="bilder/vieriet.png" type="image/png" sizes="16x16">
+
+</head>
+
+<body>
+
+
+ <?php include 'header.html';?>
+<div class="grid-container">
+<div>
+	
+</div>
+
+<main>
+	<h2>Blädderiet</h2>
+<p>
+Vivamus convallis dolor a ligula feugiat, vel tempus lectus vestibulum. Phasellus quam purus, efficitur vel consequat vel, commodo vel nulla. In at aliquet justo, a lobortis felis. Curabitur vel lacinia nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In vitae cursus tellus. Nam egestas urna eros. Maecenas pellentesque, ante sit. Mauris placerat convallis dolor sed gravida. Sed lacus diam, faucibus at purus non, faucibus mollis turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In feugiat ac leo quis blandit. Duis tempor, justo eget aliquam accumsan, sapien metus placerat felis, vitae vehicula libero augue nec dolor. Nulla facilisi. Phasellus dignissim pretium elit in congue. Morbi ultrices neque id erat facilisis consequat. Aliquam et ante orci. Proin venenatis auctor diam eget fermentum. Ut congue malesuada ex placerat maximus. Sed ut posuere nulla. Praesent vitae leo nec enim blandit porta ut at enim.
+<br /><br />
+</p>
+</main>
+
+<div>
+</div>
+</div>
+
+
+<?php include 'footer.html';?>
+
+<script src="script.js"></script>
+
+</body>
